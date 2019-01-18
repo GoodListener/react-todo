865f8c00f0129eb5116f136adabbb91672a3f2f5
ekim <good_listener@naver.com>
2019-01-18 22:47:37 +0900

    scss 추가

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import MainTemplate from './components/main/MainTemplate'
+import './App.scss'
 
 class App extends Component {
   render() {
     return (
         <React.Fragment>
-          <div gridArea="rightMenu" background="brand" />
-          <MainTemplate />
-          <div gridArea="leftMenu" background="brand" />
+          <div className="rightMenu"/>
+          <MainTemplate className="mainTemplate"/>
+          <div className="leftMenu"/>
         </React.Fragment>
     );
   }
--- src/components/user/UserForm.js
+++ src/components/user/UserForm.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import './user.scss';
 
 const UserForm = ({value, onChange, onCreate, onKeyPress}) => {
   return (
-    <div className="form">
+    <div className="userForm">
       <input value={value} onChange={onChange} onKeyPress={onKeyPress}/>
       <button onClick={onCreate}>add</button>
     </div>
