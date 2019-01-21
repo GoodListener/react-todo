e15f42526e8a991a12637d816799ffcafe03c72a
ekim <good_listener@naver.com>
2019-01-22 00:20:33 +0900

    left menu 분리

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
 import React, { Component } from 'react';
 import MainTemplate from './components/main/MainTemplate'
+import LeftMenuTemplate from './components/leftMenu/LeftMenuTemplate'
 import './App.scss'
 
 class App extends Component {
+  state = {
+    users : []
+  }
+
   render() {
     return (
         <React.Fragment>
-          <div className="layout leftMenu">LEFT</div>
-          <MainTemplate />
+          <LeftMenuTemplate
+            users={this.state.users}
+            handleCreate={this.handleCreate}
+          />
+          <MainTemplate
+            users={this.state.users}
+          />
           <div className="layout rightMenu">RIGHT</div>
         </React.Fragment>
     );
+  }
+
+  handleCreate = (input) => {
+    const { users } = this.state;
+    this.setState({
+      users: users.concat({
+        id: new Date().getTime(),
+        name: input,
+        checked: false
+      })
+    })
   }
 }
 
--- src/components/main/MainTemplate.js
+++ src/components/main/MainTemplate.js
@@ -1,58 +1,15 @@
 import React, { Component } from 'react';
 import UserList from '../user/UserList'
-import UserForm from '../user/UserForm'
 
 class MainTemplate extends Component {
-  id = 0 // 이미 0,1,2 가 존재하므로 3으로 설정
-
-  state = {
-    input: '',
-    users: []
-  }
-
   render() {
-    const {input} = this.state;
-    const {handleCreate, handleKeyPress, handleChange} = this;
     return (
       <div className="layout mainTemplate">
         <div>
-          <UserForm
-            value={input}
-            onCreate={handleCreate}
-            onKeyPress={handleKeyPress}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <UserList users={this.state.users}></UserList>
+          <UserList users={this.props.users}></UserList>
         </div>
       </div>
     );
-  }
-
-  handleCreate = () => {
-    const { input, users } = this.state;
-    this.setState({
-      input: '', // 인풋 비우고
-      // concat 을 사용하여 배열에 추가
-      users: users.concat({
-        id: this.id++,
-        name: input,
-        checked: false
-      })
-    });
-  }
-
-  handleKeyPress = (e) => {
-    if(e.key === 'Enter') {
-      this.handleCreate();
-    }
-  }
-
-  handleChange = (e) => {
-    this.setState({
-      input: e.target.value // input 의 다음 바뀔 값
-    });
   }
 }
 
